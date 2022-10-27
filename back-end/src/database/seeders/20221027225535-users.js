9f97492f837c1e783219055bd309c6436ad36701
Maykon Freitas <maykon17programmer@gmail.com>
2022-10-27 20:26:48 -0300

    feat: adiciona o arquivo seeds products

--- back-end/src/database/seeders/20221027225535-users.js
+++ back-end/src/database/seeders/20221027225535-users.js
@@ -24,7 +24,7 @@ module.exports = {
         password: '1c37466c159755ce1fa181bd247cb925',
         role: 'customer',
       },
-    ], {});
+    ], { timestamp: false });
   },
 
   async down (queryInterface, Sequelize) {
