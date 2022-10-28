6fc2678bc391c2eb86fd1305f01eede300df2538
Vitor Soares <pereiravitor1218@gmail.com>
2022-10-28 15:46:47 -0300

    feat: login validadeForm

--- front-end/src/pages/Login.jsx
+++ front-end/src/pages/Login.jsx
@@ -1,7 +1,81 @@
+import { useEffect, useState } from 'react';
+import logo from '../assets/logo.png';
+
 export default function Login() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [isValidForm, setIsValidForm] = useState(false);
+  const [errorLoginMessage, setErrorLoginMessage] = useState(false);
+
+  function handleValidadeForm() {
+    const regexEmail = /^\S+@\S+.\S+$/;
+    const minLengthPass = 6;
+    const validadeEmail = regexEmail.test(email);
+    const validadePass = password.length >= minLengthPass;
+    const isValid = validadeEmail && validadePass;
+    setIsValidForm(isValid);
+  }
+
+  useEffect(() => {
+    handleValidadeForm();
+  }, [email, password]);
+
   return (
     <div>
-      Login
+      <div>
+        <img src={ logo } alt="" />
+        <p>Ze delivery</p>
+      </div>
+      <div>
+        <form>
+          <label htmlFor="email">
+            Login
+            <input
+              type="email"
+              id="email"
+              name="email"
+              data-testid="common_login__input-email"
+              value={ email }
+              onChange={ ({ target }) => setEmail(target.value) }
+            />
+          </label>
+
+          <label htmlFor="password">
+            Senha
+            <input
+              type="password"
+              id="password"
+              name="password"
+              data-testid="common_login__input-password"
+              value={ password }
+              onChange={ (e) => setPassword(e.target.value) }
+            />
+          </label>
+
+          <button
+            type="submit"
+            data-testid="common_login__button-login"
+            disabled={ !isValidForm }
+          >
+            Login
+          </button>
+          <button
+            type="button"
+            data-testid="common_login__button-register"
+          >
+            Ainda não tenho conta
+          </button>
+        </form>
+      </div>
+      <div>
+        {errorLoginMessage && (
+          <p
+            data-testid="common_login__element-invalid-email"
+          >
+            Email ou senha mal formatados
+          </p>
+        )}
+      </div>
     </div>
   );
 }
