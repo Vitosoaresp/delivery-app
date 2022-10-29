6e3aecaca6874103bcd549b05b81037a53f2d555
Mayumi <cinthya.m.kuniyoshi@gmail.com>
2022-10-29 10:13:42 -0300

    adiciona saleProduct.model.js referente ao model do saleProduct

---
+++ back-end/src/database/models/saleProduct.model.js
@@ -0,0 +1,29 @@
+module.exports = (sequelize, DataTypes) => {
+  const SaleProduct = sequelize.define('SaleProduct', {
+    saleId: {
+      type: DataTypes.INTEGER,
+      foreignKey: true,
+    },
+    productId: {
+      type: DataTypes.INTEGER,
+      foreignKey: true,
+    },
+    quantity: {
+      type: DataTypes.INTEGER
+    }
+  },
+  {
+    timestamps: false,
+    tableName: 'salesProducts',
+    underscored: true,
+  });
+
+  SaleProduct.associate = (models) => {
+    SaleProduct.belongsTo(models.Sale,
+      { foreignKey: 'sale_id', as: 'sales' });
+    SaleProduct.belongsTo(models.Product,
+      { foreignKey: 'product_id', as: 'products' });
+  };
+
+  return SaleProduct;
+};
