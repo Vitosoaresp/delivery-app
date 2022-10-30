1b56979b83923a39e1f07fb464beba8905602b14
Vitor Soares <pereiravitor1218@gmail.com>
2022-10-30 18:19:35 -0300

    test: mocks login data

---
+++ front-end/src/tests/mocks/loginMocks.js
@@ -0,0 +1,6 @@
+const emailMock = 'fulana@deliveryapp.com';
+const passwordMock = 'fulana@123';
+const emailFailMock = 'test@gmail';
+const passwordFailMock = '1234';
+
+export { emailMock, passwordMock, emailFailMock, passwordFailMock };
