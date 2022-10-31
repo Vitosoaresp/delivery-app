788e5a5fa75ceb4537e26f65082a2afa0ae93893
Maykon Freitas <maykon17programmer@gmail.com>
2022-10-31 16:46:25 -0300

    test: adiciona os testes relacionados ao service do user - função create

--- back-end/src/tests/unit/services/user.service.test.js
+++ back-end/src/tests/unit/services/user.service.test.js
@@ -8,8 +8,8 @@ userMock = {
   id: 1,
   name: 'Delivery App Admin',
   email: 'adm@deliveryapp.com',
-  password: 'a4c86edecc5aee06eff8fdeda69e0d04',
-  role: 'administrator',
+  password: 'a4c86edecc5aee06eff8fdeda69e0d04', // --adm2@21!!--
+  role: 'customer',
 }
 
 describe('Testes de unidade do service de users', function () {
@@ -25,17 +25,18 @@ describe('Testes de unidade do service de users', function () {
     });
   });
 
-  // describe('teste do endpoint /register', function () {
-  //   it('cadastro realizado com sucesso', async function () {
-  //     sinon.stub(UserService, 'create').resolves(userMock);
-
-  //     const email = userMock.email;
-  //     const password = userMock.password;
-  //     const result = await UserService.create(email, password);
-  
-  //     expect(result).to.be.deep.equal(userMock);
-  //   });
-  // });
+  describe('teste do endpoint /register', function () {
+    it('cadastro realizado com sucesso', async function () {
+      sinon.stub(User, 'findOne').resolves(null);
+      sinon.stub(User, 'create').resolves(userMock);
+      const result = await UserService.create({
+        name: userMock.name,
+        email: userMock.email,
+        password: '--adm2@21!!--',
+      });
+      expect(result).to.be.deep.equal(userMock);
+    });
+  });
 
 
   afterEach(sinon.restore);
