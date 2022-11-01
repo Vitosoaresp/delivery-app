8e5d82b89fc9881ea86240736870bafd556664f6
davi <davijluna@gmail.com>
2022-11-01 11:05:58 -0300

    fix:correção de criptografia no arquivo util/jwt.js

--- back-end/src/utils/jwt.js
+++ back-end/src/utils/jwt.js
@@ -1,17 +1,19 @@
 const jwt = require('jsonwebtoken');
+// utilização de metodo mostrado no slack por outro grupo.
+const jwtkey = require('fs').readFileSync('../back-end/jwt.evaluation.key', { encoding: 'utf-8' }); 
 
-const key = 'secret_key';
+// const key = 'secret_key';
 
 const jwtService = {
   createToken: (data) => {
-    const token = jwt.sign({ data }, key);
+    const token = jwt.sign({ data }, jwtkey); // refatoração trocando a constante key por jwtkey 
     return token;
   },
 
   validateToken: (token) => {
     try {
       if (!token) return { error: { code: 401, message: { message: 'Token not found' } } };
-      const data = jwt.verify(token, key);
+      const data = jwt.verify(token, jwtkey); // refatoração trocando a constante key por jwtkey 
       return data;
     } catch (e) {
       return { error: { code: 401, message: { message: 'Expired or invalid token' } } }; 
