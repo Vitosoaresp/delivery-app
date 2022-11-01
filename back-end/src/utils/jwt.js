6f21526c935d3f2ac811b2a03b57377ea57e7602
davi <davijluna@gmail.com>
2022-11-01 14:01:02 -0300

    fix:retirando comentatios

--- back-end/src/utils/jwt.js
+++ back-end/src/utils/jwt.js
@@ -1,19 +1,16 @@
 const jwt = require('jsonwebtoken');
-// utilização de metodo mostrado no slack por outro grupo.
 const jwtkey = require('fs').readFileSync('../back-end/jwt.evaluation.key', { encoding: 'utf-8' }); 
-
-// const key = 'secret_key';
 
 const jwtService = {
   createToken: (data) => {
-    const token = jwt.sign({ data }, jwtkey); // refatoração trocando a constante key por jwtkey 
+    const token = jwt.sign({ data }, jwtkey);
     return token;
   },
 
   validateToken: (token) => {
     try {
       if (!token) return { error: { code: 401, message: { message: 'Token not found' } } };
-      const data = jwt.verify(token, jwtkey); // refatoração trocando a constante key por jwtkey 
+      const data = jwt.verify(token, jwtkey);
       return data;
     } catch (e) {
       return { error: { code: 401, message: { message: 'Expired or invalid token' } } }; 
