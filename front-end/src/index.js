e3b1f12bac0f092e78133075e68eafb40da4e0f2
Vitor Soares <pereiravitor1218@gmail.com>
2022-11-01 16:59:04 -0300

    feat: use context in index.js

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -2,12 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
+import DeliveryContextProvider from './context/DeliveryContext';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <DeliveryContextProvider>
+        <App />
+      </DeliveryContextProvider>
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root'),
