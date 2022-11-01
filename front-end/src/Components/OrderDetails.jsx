d2dba29a717048924134d47be7ae20298f2a079e
davi <davijluna@gmail.com>
2022-11-01 20:40:09 -0300

    feat: adiciona página checkout de produtos

--- front-end/src/Components/OrderDetails.jsx
+++ front-end/src/Components/OrderDetails.jsx
@@ -1,51 +1,34 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-export default function OrderDetails({ name, cost }) {
-  const number = 3;
+export default function OrderDetails({ index, quantity, name, cost }) {
   return (
-    <div>
-      <table>
-        <tr>
-          <th>Item</th>
-          <th>Descrição</th>
-          <th>Quantidade</th>
-          <th>Valor Unitario</th>
-          <th>Sub-total</th>
-          <th>Remover Item</th>
-        </tr>
-        <tr>
-          <td data-testid={ `customer_checkout__element-order-table-item-number${id}` }>
-            1
-          </td>
+    <tr>
+      <td data-testid={ `customer_checkout__element-order-table-item-number-${index}` }>
+        {index + 1}
+      </td>
 
-          <td data-testid={ `customer_checkout__element-order-table-name${id}` }>
-            { name }
+      <td data-testid={ `customer_checkout__element-order-table-name-${index}` }>
+        {name}
 
-          </td>
+      </td>
 
-          <td data-testid={ `customer_checkout__element-order-table-quantity${id}` }>
-            3
-          </td>
+      <td data-testid={ `customer_checkout__element-order-table-quantity-${index}` }>
+        {quantity}
+      </td>
 
-          <td data-testid={ `customer_checkout__element-order-table-unit-price${id}` }>
-            {cost}
-          </td>
+      <td data-testid={ `customer_checkout__element-order-table-unit-price-${index}` }>
+        {cost.replace('.', ',')}
+      </td>
 
-          <td data-testid={ `customer_checkout__element-order-table-sub-total-${id}` }>
-            {number * cost}
-          </td>
+      <td data-testid={ `customer_checkout__element-order-table-sub-total-${index}` }>
+        {Number(quantity * cost).toFixed(2).replace('.', ',')}
+      </td>
 
-          <td data-testid="customer_checkout__element-order-table-remove">
-            <button type="button">Remover</button>
-          </td>
-        </tr>
-      </table>
-      <p data-testid="customer_checkout__element-order-total-price">
-        { `Total: R$ ${subtotal.reduce((acc, curr) => acc + curr.subtotal, 0)} `}
-
-      </p>
-    </div>
+      <td data-testid={ `customer_checkout__element-order-table-remove-${index}` }>
+        <button type="button">Remover</button>
+      </td>
+    </tr>
   );
 }
 
@@ -53,4 +36,6 @@ OrderDetails.propTypes = {
   id: PropTypes.string,
   name: PropTypes.string,
   cost: PropTypes.string,
+  quantity: PropTypes.string,
+  index: PropTypes.number,
 }.isRequired;
--- front-end/src/pages/Checkout.jsx
+++ front-end/src/pages/Checkout.jsx
@@ -1,24 +1,58 @@
+import React, { useContext } from 'react';
+
 import Navbar from '../Components/Navbar';
-// import OrderDetails from '../Components/OrderDetails';
+import OrderDetails from '../Components/OrderDetails';
+import { DeliveryContext } from '../context/DeliveryContext';
 
 export default function Checkout() {
+  const { cart } = useContext(DeliveryContext);
+
+  const totalPrice = cart.reduce((acc, { cost, quantity }) => {
+    const price = cost.replace(',', '.');
+    return acc + (Number(price) * quantity);
+  }, 0);
+
   return (
     <div>
       <Navbar />
-      <container>
-        <h1>Finalizar Pedido</h1>
-        {/* <OrderDetails /> */}
-        <select data-testid="customer_checkoutselect-seller">
-          <option value="ss">ss</option>
-          <option value="adasd">adasd</option>
-          <option value="sdads">sdads</option>
-        </select>
-        <input type="text" data-testid="customer_checkoutinput-address" />
-        <input type="text" data-testid="customer_checkoutinput-address-number" />
-        <button type="submit" data-testid="customer_checkoutbutton-submit-order">
-          Finalizar Pedido
-        </button>
-      </container>
+      <h1>Finalizar Pedido</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>Item</th>
+            <th>Descrição</th>
+            <th>Quantidade</th>
+            <th>Valor Unitario</th>
+            <th>Sub-total</th>
+            <th>Remover Item</th>
+          </tr>
+        </thead>
+        <tbody>
+          {cart.map((product, index) => (
+            <OrderDetails
+              id={ product.id }
+              key={ product.id }
+              cost={ product.cost }
+              name={ product.name }
+              quantity={ product.quantity }
+              index={ index }
+            />
+          ))}
+        </tbody>
+      </table>
+      <p data-testid="customer_checkout__element-order-total-price">
+        {totalPrice.toFixed(2).replace('.', ',')}
+      </p>
+      <select data-testid="customer_checkout__select-seller">
+        <option value="ss">ss</option>
+        <option value="adasd">adasd</option>
+        <option value="sdads">sdads</option>
+      </select>
+      <input type="text" data-testid="customer_checkout__input-address" />
+      <input type="text" data-testid="customer_checkout__input-address-number" />
+      <button type="submit" data-testid="customer_checkout__button-submit-order">
+        Finalizar Pedido
+      </button>
     </div>
   );
 }
