e3475189d3fc76838fd9931ab0d2d92d4286e674
Vitor Soares <pereiravitor1218@gmail.com>
2022-11-01 17:00:01 -0300

    feat: quantity and change price render

--- front-end/src/Components/CardProduct.jsx
+++ front-end/src/Components/CardProduct.jsx
@@ -1,12 +1,14 @@
-import React from 'react';
 import PropTypes from 'prop-types';
+import React, { useState } from 'react';
 
 export default function CardProduct({ id, cost, name, thumb }) {
+  const [quantity, setQuantity] = useState(0);
+
   return (
     <div key={ id }>
       <div>
         <div data-testid={ `customer_products__element-card-price-${id}` }>
-          {`R$ ${cost}`}
+          {cost.replace('.', ',')}
         </div>
         <img
           alt="drink"
@@ -25,16 +27,21 @@ export default function CardProduct({ id, cost, name, thumb }) {
         <button
           type="button"
           data-testid={ `customer_products__button-card-rm-item-${id}` }
+          onClick={ () => setQuantity(quantity > 0 ? quantity - 1 : 0) }
         >
-          {' '}
           -
         </button>
-        <input data-testid={ `customer_products__input-card-quantity-${id}` } />
+        <input
+          data-testid={ `customer_products__input-card-quantity-${id}` }
+          type="number"
+          min="0"
+          value={ quantity }
+        />
         <button
           type="button"
           data-testid={ `customer_products__button-card-add-item-${id}` }
+          onClick={ () => setQuantity(quantity + 1) }
         >
-          {' '}
           +
         </button>
       </div>
