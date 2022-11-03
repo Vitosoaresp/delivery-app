e560eea820ea575acbb038a8a70f609334143871
Maykon Freitas <maykon17programmer@gmail.com>
2022-11-03 20:48:51 -0300

    feat: adiciona o metodo getSellers no arquivo user.controller.js

--- back-end/src/controllers/user.controller.js
+++ back-end/src/controllers/user.controller.js
@@ -22,6 +22,11 @@ const userController = {
     
     return res.status(201).json(user);
   },
+
+  getSellers: async (_req, res) => {
+    const sellers = await userService.getSellers();
+    return res.status(200).json(sellers);
+  },
 };
 
 module.exports = userController;
