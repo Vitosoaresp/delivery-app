65f14f9cba095daea605604a32963e9d679ec3a1
davi <davijluna@gmail.com>
2022-11-03 16:22:54 -0300

    feat: adiciona moment.js as dependencias do projeto e formato correto da data

--- back-end/src/services/sale.service.js
+++ back-end/src/services/sale.service.js
@@ -1,11 +1,13 @@
+const moment = require('moment');
 // const jwtService = require('../utils/jwt');
 
 const { Sale } = require('../database/models');
 
 const saleService = {
   create: async (data) => {
-    const { userId, totalPrice, deliveryAddress, delineryNumber, saleDate } = data;
+    const { userId, totalPrice, deliveryAddress, delineryNumber } = data;
     const status = 'Pendente';
+    const saleDate = moment().format('L, LTS');
     const newSale = await Sale.create({
     userId, sellerId: 2, totalPrice, deliveryAddress, delineryNumber, saleDate, status,
     });
