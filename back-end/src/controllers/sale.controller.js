365ddae411d8ceeffdf702b56c8a048548b5ae2c
mayukuni <marcelobjr@DESKTOP-9NEFQHU.localdomain>
2022-11-04 16:17:50 -0300

    feat: adiciona mudancas no saleController

--- back-end/src/controllers/sale.controller.js
+++ back-end/src/controllers/sale.controller.js
@@ -2,8 +2,9 @@ const saleService = require('../services/sale.service');
 
 const saleController = {
   create: async (req, res) => {
-    const { data } = req.user;
-    const sale = await saleService.create({ userId: data.id, ...req.body });    
+    // const { data } = req.user;
+    console.log(req.headers.authorization);
+    const sale = await saleService.create({ userId: 1, ...req.body.data });    
     return res.status(201).json(sale);
   },
 };
