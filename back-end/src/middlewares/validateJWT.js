a902afc568398af7a3053ef3a447b066fe14c190
Maykon Freitas <maykon17programmer@gmail.com>
2022-11-04 21:23:09 -0300

    feat: finaliza o requisito 20 do projeto

--- back-end/src/middlewares/validateJWT.js
+++ back-end/src/middlewares/validateJWT.js
@@ -2,7 +2,7 @@ const jwt = require('jsonwebtoken');
 const jwtkey = require('fs').readFileSync('../back-end/jwt.evaluation.key', { encoding: 'utf-8' }); 
 
 module.exports = async (req, res, next) => {
-  const token = req.header('Authorization');
+  const token = req.headers.authorization;
   if (!token) return res.status(404).json({ message: 'Token not found' });
   try {
     const data = jwt.verify(token, jwtkey);
