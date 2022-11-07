3d049fdf57775abe3937b4cece39be835ef5e6b9
Maykon Freitas <maykon17programmer@gmail.com>
2022-11-07 18:06:37 -0300

    fix: resolve erro desnecesário

--- back-end/src/database/models/saleProduct.model.js
+++ back-end/src/database/models/saleProduct.model.js
@@ -1,13 +1,5 @@
 module.exports = (sequelize, DataTypes) => {
     const SaleProduct = sequelize.define('SaleProduct', {
-      saleId: {
-        type: DataTypes.INTEGER,
-        primaryKey: true,
-      },
-      productId: {
-        type: DataTypes.INTEGER,
-        primaryKey: true,
-      },
       quantity: {
         type: DataTypes.INTEGER
       }
@@ -19,11 +11,19 @@ module.exports = (sequelize, DataTypes) => {
     });
   
     SaleProduct.associate = (models) => {
-      console.log(models);
-      models.Sale.belongsToMany(models.Product,
-        { as: 'sales', through: SaleProduct, foreignKey: 'saleId', otherKey: 'productId' });
-      models.Product.belongsToMany(models.Sale,
-        { as: 'products', through: SaleProduct, foreignKey: 'productId', otherKey: 'saleId' });
+      models.Sale.belongsToMany(models.Product, {
+        as: 'sales',
+        through: SaleProduct,
+        foreignKey: 'saleId',
+        otherKey: 'productId'
+      });
+
+      models.Product.belongsToMany(models.Sale, {
+        as: 'products',
+        through: SaleProduct,
+        foreignKey: 'productId',
+        otherKey: 'saleId'
+      });
     };
     
     return SaleProduct;
