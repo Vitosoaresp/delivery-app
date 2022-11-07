39dd89344d250b61dba6ba94fb0bcd73af4f3a33
Vitor Soares <pereiravitor1218@gmail.com>
2022-11-07 14:26:48 -0300

    fix: token headers

--- back-end/src/middlewares/validateJWT.js
+++ back-end/src/middlewares/validateJWT.js
@@ -1,14 +1,14 @@
 const jwt = require('jsonwebtoken');
-const jwtkey = require('fs').readFileSync('../back-end/jwt.evaluation.key', { encoding: 'utf-8' }); 
+const jwtkey = require('fs').readFileSync('../back-end/jwt.evaluation.key', { encoding: 'utf-8' });
 
 module.exports = async (req, res, next) => {
-  const token = req.header('Authorization');
+  const token = req.headers.authorization;
   if (!token) return res.status(404).json({ message: 'Token not found' });
   try {
     const data = jwt.verify(token, jwtkey);
     req.user = data;
     next();
   } catch (e) {
-    return res.status(401).json({ message: 'Expired or invalid token' }); 
+    return res.status(401).json({ message: 'Expired or invalid token' });
   }
 };
