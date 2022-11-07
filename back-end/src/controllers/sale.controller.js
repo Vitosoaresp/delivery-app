da74480b6cea77de9b9232b6255b37815ed0f21a
mayukuni <marcelobjr@DESKTOP-9NEFQHU.localdomain>
2022-11-07 18:53:21 -0300

    feat: adiciona alteracoes no arquivo sale.controller.js

--- back-end/src/controllers/sale.controller.js
+++ back-end/src/controllers/sale.controller.js
@@ -2,8 +2,8 @@ const saleService = require('../services/sale.service');
 
 const saleController = {
   create: async (req, res) => {
-    const { data: { userId } } = req.user;
-    const sale = await saleService.create({ userId, ...req.body });
+    const { data: { id } } = req.user;
+    const sale = await saleService.create({ userId: id, ...req.body });
     return res.status(201).json(sale);
   },
 };
