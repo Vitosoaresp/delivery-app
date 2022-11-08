e147b72e1a784aee9c2f144ace6a9d74db33062a
Maykon Freitas <maykon17programmer@gmail.com>
2022-11-08 16:53:25 -0300

    test: adiciona testes relacionando a função getBySeller do sale.controller.js

--- back-end/src/tests/unit/controllers/sale.controller.test.js
+++ back-end/src/tests/unit/controllers/sale.controller.test.js
@@ -5,7 +5,7 @@ const { Sale } = require('../../../database/models');
 const SaleService = require('../../../services/sale.service');
 const SaleController = require('../../../controllers/sale.controller');
 
-saleMock = { 
+const saleMock = { 
   userId: 1,
   sellerId: 2,
   totalPrice: 10.0,
@@ -14,10 +14,14 @@ saleMock = {
   saleDate: '03/11/2022 15:47',
 }
 
-saleMockWithId = {
-  data: {
-    id: 1,
-  }
+const saleMockWithId = {
+  id: 1,
+  userId: 1,
+  sellerId: 2,
+  totalPrice: 10.0,
+  deliveryAddress: 'rua 2',
+  delineryNumber: '121',
+  saleDate: '03/11/2022 15:47',
 }
 
 describe('Testes de unidade do controller de sales', function () {
@@ -27,21 +31,38 @@ describe('Testes de unidade do controller de sales', function () {
 
     before(async () => {
       req.body = saleMock;
-      req.user = saleMockWithId;
+      req.user = { data: { id: 1 }};
 
       res.status = sinon.stub().returns(res);
       res.json = sinon.stub().returns(res);
     });
 
     it('cadastro realizado com sucesso', async function () {
-      sinon.stub(SaleService, 'create').resolves({ id: 1 });
+      sinon.stub(SaleService, 'create').resolves(saleMockWithId);
       await SaleController.create(req, res);
 
       expect(res.status.calledWith(201)).to.be.true;
-      expect(res.json.calledWith({ id: 1 })).to.be.true;
+      expect(res.json.calledWith(saleMockWithId)).to.be.true;
     });
   });
 
+  describe('teste do endpoint /sales/seller/:id', function () {
+    const req = {};
+    const res = {};
+
+    before(async () => {
+      res.status = sinon.stub().returns(res);
+      res.json = sinon.stub().returns(res);
+    });
+
+    it('busca realizada com sucesso', async function () {
+      sinon.stub(SaleService, 'getBySeller').resolves([saleMockWithId]);
+      await SaleController.getBySeller(req, res);
+
+      expect(res.status.calledWith(200)).to.be.true;
+      expect(res.json.calledWith([saleMockWithId])).to.be.true;
+    });
+  });
 
   afterEach(sinon.restore);
 });
