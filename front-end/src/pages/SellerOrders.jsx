9779d1c7f0d150ddcfd94b8794150d7147451fa8
Maykon Freitas <maykon17programmer@gmail.com>
2022-11-08 15:30:36 -0300

    feat: adiciona o arquivo SellerOrders.jsx - responsavel por configurar a página da rota /seller/orders

---
+++ front-end/src/pages/SellerOrders.jsx
@@ -0,0 +1,24 @@
+import React, { useContext, useState } from 'react';
+// import { useHistory } from 'react-router-dom';
+
+// import Navbar from '../Components/Navbar';
+// import OrderDetails from '../Components/OrderDetails';
+// import { DeliveryContext } from '../context/DeliveryContext';
+// import createSale from '../services/createSales';
+// import createSaleProduct from '../services/CreateSalesProducts';
+
+export default function Checkout() {
+  //   const { cart, sellers } = useContext(DeliveryContext);
+  //   const [deliveryAddress, setDeliveryAddress] = useState('');
+  //   const [deliveryNumber, setDeliveryNumber] = useState('');
+  //   const [seller, setSeller] = useState(2);
+  //   const history = useHistory();
+
+  // const { token } = JSON.parse(localStorage.getItem('user'));
+
+  return (
+    <div>
+      Bem-vindo, Vendedor!
+    </div>
+  );
+}
