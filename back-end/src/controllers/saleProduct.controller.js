d53c22d9917b5d1b428b191d6a235c9d4380226f
Maykon Freitas <maykon17programmer@gmail.com>
2022-11-08 16:42:02 -0300

    test/fix: refatora os testes da função create do saleProduct.controller.test

--- back-end/src/controllers/saleProduct.controller.js
+++ back-end/src/controllers/saleProduct.controller.js
@@ -2,7 +2,6 @@ const saleProductService = require('../services/saleProduct.service');
 
 const saleProductController = {
   create: async (req, res) => {
-    console.log(req.body);
     const saleProduct = await saleProductService.create(req.body);
     return res.status(201).json(saleProduct);
   },
