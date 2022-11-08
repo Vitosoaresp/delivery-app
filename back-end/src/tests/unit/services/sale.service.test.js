c7da7731eaf37fc9ff043ab4f40fe372b729e6e6
Maykon Freitas <maykon17programmer@gmail.com>
2022-11-08 16:36:43 -0300

    test/fix: modifica os testes da função create do sale.service.js

--- back-end/src/tests/unit/services/sale.service.test.js
+++ back-end/src/tests/unit/services/sale.service.test.js
@@ -3,6 +3,11 @@ const sinon = require('sinon');
 
 const { Sale } = require('../../../database/models');
 const SaleService = require('../../../services/sale.service');
+const saleProductService = require('../../../services/saleProduct.service');
+
+const cart = [
+  { id: 1, name: 'Skol Lata 250ml', quantity: 10 },
+];
 
 const sale = { 
   userId: 1,
@@ -11,16 +16,34 @@ const sale = {
   deliveryAddress: 'rua 2',
   delineryNumber: '121',
   saleDate: '03/11/2022 15:47',
+  cart,
+}
+
+const saleWithId = {
+  id: 1,
+  userId: 1,
+  sellerId: 2,
+  totalPrice: 10.0,
+  deliveryAddress: 'rua 2',
+  delineryNumber: '121',
+  saleDate: '03/11/2022 15:47',
+}
+
+const saleProduct = {
+  saleId: 1,
+  productId: 1,
+  quantity: 10,
 }
 
 describe('Testes de unidade do service de Sales', function () {
   describe('teste do endpoint /sales', function () {
     it('Sale criado com sucesso', async function () {
-      sinon.stub(Sale, 'create').resolves({ id: 1 });
-      const result = await SaleService.create(sale);
-  
-      expect(result).to.be.deep.equal({ id: 1 });
-      expect(result).to.be.an('object');
+      sinon.stub(Sale, 'create').resolves(saleWithId);
+      sinon.stub(saleProductService, 'create').resolves(saleProduct);
+      
+      const result = SaleService.create(sale);
+
+      expect(await result).to.be.deep.equal(saleWithId);
     });
   });
 
