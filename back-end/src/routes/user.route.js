dea5b5ef71abb471bfbffbcd023095ffc0a2c1b2
mayukuni <marcelobjr@DESKTOP-9NEFQHU.localdomain>
2022-11-09 20:06:47 -0300

    feat: adiciona rota /users no user.route.js

--- back-end/src/routes/user.route.js
+++ back-end/src/routes/user.route.js
@@ -4,6 +4,7 @@ const userController = require('../controllers/user.controller');
 const router = Router();
 
 router.get('/users/sellers', userController.getSellers);
+router.get('/users', userController.getAll);
 router.post('/login', userController.login);
 router.post('/register', userController.create);
 
