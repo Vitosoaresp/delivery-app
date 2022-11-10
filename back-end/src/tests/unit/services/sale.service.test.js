eaa5bd06e6bc3d09a8ceb41933204c8864eddab1
Maykon Freitas <maykon17programmer@gmail.com>
2022-11-10 20:01:58 -0300

    test: adiciona testes da função getAllByUserId do sale.service.js

--- back-end/src/tests/unit/services/sale.service.test.js
+++ back-end/src/tests/unit/services/sale.service.test.js
@@ -79,16 +79,27 @@ describe('Testes de unidade do service de Sales', function () {
     });
   });
 
-    // Method getById
-    describe('teste do endpoint /sales/:id', function () {
-      it('Busca de Sales por sellerId feito com sucesso', async function () {
-        sinon.stub(Sale, 'findByPk').resolves(saleWithId);
-        const result = await SaleService.getById(1);
-    
-        expect(result).to.be.deep.equal(saleWithId);
-        expect(result).to.be.an('object');
-      });
+  // Method getById
+  describe('teste do endpoint /sales/:id', function () {
+    it('Busca de Sales por sellerId feito com sucesso', async function () {
+      sinon.stub(Sale, 'findByPk').resolves(saleWithId);
+      const result = await SaleService.getById(1);
+  
+      expect(result).to.be.deep.equal(saleWithId);
+      expect(result).to.be.an('object');
     });
+  });
+
+  // Method getAllByUserId
+  describe('teste do endpoint /sales', function () {
+    it('Busca de Sales pelo id do user feito com sucesso', async function () {
+      sinon.stub(Sale, 'findAll').resolves([saleWithId]);
+      const result = await SaleService.getAllByUserId(1);
+  
+      expect(result).to.be.deep.equal([saleWithId]);
+      expect(result).to.be.an('array');
+    });
+  });
 
   afterEach(sinon.restore);
 });
